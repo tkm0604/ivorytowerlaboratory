26ba1688a6010723346704dd6c5112546ead6a38
yamaguchi.takumi <gu.jp06042gmail.com>
2023-11-14 17:51:04 +0900

    動的ファイル追加

---
+++ dist/html/ivorytowerlab/wp-content/themes/ivorytowerlab/footer.php
@@ -0,0 +1,18 @@
+<footer class="l-footer">
+
+<img class="p-footer-logo" src="/assets/img/common/ivory_tower_lab_logo.png" alt="">
+
+    <div class="p-footer-bottom">
+        <p class="p-footer-bottom__copyright">© Takumi Yamaguchi</p>
+    </div>
+
+    <a id="pagetop" href="javascript:void(0);"  class="page_top" style="">
+        <i class="fa fa-arrow-up fa-3x" aria-hidden="true"></i>
+    </a>
+
+</footer>
+
+  </div>
+</body>
+
+</html>
---
+++ dist/html/ivorytowerlab/wp-content/themes/ivorytowerlab/header.php
@@ -0,0 +1,75 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+<meta charset="UTF-8">
+<meta name="description" content="">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<meta name="format-detection" content="telephone=no">
+<title>Ivory Tower Laboratoryのポートフォリオサイト</title>
+<meta property="og:title" content="Ivory Tower Laboratoryのポートフォリオサイト">
+<meta property="og:description" content="">
+<meta property="og:image" content="">
+<meta property="og:type" content="website">
+<meta property="og:url" content="">
+<meta property="og:locate" content="ja_JP">
+<meta property="og:site_name" content="">
+<link rel="icon" href="/assets/img/common/favicon.ico">
+<link rel="apple-touch-icon" href="/assets/img/common/apple-touch-icon.png">
+<link rel="manifest" href="/assets/img/common/manifest.webmanifest">
+<link rel="preconnect" href="https://fonts.googleapis.com">
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+<link href="https://fonts.googleapis.com/css2?family=Noto+Sans+JP:wght@400;700&family=Maven+Pro:wght@600&display=swap" rel="stylesheet">
+<link rel="stylesheet" href="/assets/css/styles.css" media="all">
+<link href=”https://use.fontawesome.com/releases/v6.0.0/css/all.css” rel=”stylesheet”>
+<script defer src="/assets/js/bundle.js"></script>
+<script src="https://kit.fontawesome.com/c143f5855b.js" crossorigin="anonymous"></script>
+
+</head>
+
+<body>
+  <div class="l-wrapper">
+    <header id="main" class="l-header">
+    <div class="p-header-inner">
+        <h1 class="p-header-inner__title">
+            <a class="p-header-inner__link" href="https://ivorytowerlab.jp/">
+                <img class="p-header-inner__logo" src="/assets/img/common/ivory_tower_lab_logo.png">
+            </a>
+        </h1>
+
+        <nav class="p-header-list">
+
+                <ul class="p-header-list__menu">
+                    <li class="p-header-list__item">
+                        <a class="p-header-list__link" href="#about-me">About Me</a>
+                    </li>
+                    <li class="p-header-list__item">
+                        <a class="p-header-list__link" href="#works">Works</a>
+                    </li>
+                    <li class="p-header-list__item">
+                        <a class="p-header-list__link" href="#contact">Contact</a>
+                    </li>
+                </ul>
+
+                <span id="nav_toggle" class="nav_toggle">
+                    <i></i>
+                    <i></i>
+                    <i></i>
+                </span>
+
+                <div class="p-sp-menu">
+                    <ul id="sp-menu__list" class="p-sp-menu__item">
+                        <li class="p-sp-menu__item">
+                            <a class="p-sp-menu__link" href="#about-me">About Me</a>
+                        </li>
+                        <li class="p-sp-menu__item">
+                            <a class="p-sp-menu__link" href="#works">Works</a>
+                        </li>
+                        <li class="p-sp-menu__item">
+                            <a class="p-sp-menu__link" href="#contact">Contact</a>
+                        </li>
+                    </ul>
+                </div>
+
+        </nav>
+    </div>
+</header>
