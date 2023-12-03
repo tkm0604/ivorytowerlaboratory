6fb63b25462338a0e813f8372a9e79f5152bf5a9
yamaguchi.takumi <gu.jp06042gmail.com>
2023-12-03 10:49:23 +0900

    form入力、確認、送信までの動作処理を追加

--- dist/html/ivorytowerlab/wp-content/themes/ivorytowerlab/front-page.php
+++ dist/html/ivorytowerlab/wp-content/themes/ivorytowerlab/front-page.php
@@ -1,3 +1,59 @@
+<?php
+
+session_start();
+
+// 'formStatus'が設定されていない場合に初期化
+if (!isset($_SESSION['formStatus'])) {
+  $_SESSION['formStatus'] = 'input'; // デフォルトの状態
+}
+
+// フォームデータをセッションに保存
+function saveFormData() {
+    if (!empty($_POST)) {
+        foreach ($_POST as $key => $value) {
+            $_SESSION['formData'][$key] = $value;
+        }
+    }
+}
+
+// POSTリクエストがない場合は、フォーム状態をリセット
+if ($_SERVER["REQUEST_METHOD"] != "POST") {
+  if (isset($_SESSION['formStatus']) && $_SESSION['formStatus'] == 'complete') {
+    $_SESSION['formStatus'] = 'input';
+    // セッションデータをクリア
+    unset($_SESSION['formData']);
+  }
+}
+
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+  saveFormData(); // フォームデータを保存
+  if (isset($_POST["submitConfirm"])) {
+    $_SESSION['formStatus'] = 'confirm';
+  } elseif (isset($_POST["submitBack"])) {
+    $_SESSION['formStatus'] = 'input';
+  } elseif (isset($_POST["submitFinal"])) {
+    $_SESSION['formStatus'] = 'complete';
+  }
+}
+
+// フォームデータを取得するためのヘルパー関数
+function getSavedValue($key) {
+    return isset($_SESSION['formData'][$key]) ? $_SESSION['formData'][$key] : '';
+}
+
+$companyName = isset($_POST['company-name']) ? $_POST['company-name'] : '';
+$pic = isset($_POST['pic']) ? $_POST['pic'] : '';
+$picKana = isset($_POST['pic-kana']) ? $_POST['pic-kana'] : '';
+$email = isset($_POST['email']) ? $_POST['email'] : '';
+$tel1 = isset($_POST['tel-1']) ? $_POST['tel-1'] : '';
+$tel2 = isset($_POST['tel-2']) ? $_POST['tel-2'] : '';
+$tel3 = isset($_POST['tel-3']) ? $_POST['tel-3'] : '';
+$msg = isset($_POST['msg']) ? $_POST['msg'] : '';
+
+var_dump($_SESSION);
+print '<br><br>';
+var_dump($_POST);
+?>
 <?php get_header(); ?>
 
 <main class="l-main">
@@ -87,27 +143,27 @@
               <?php if (has_post_thumbnail()) : ?>
                 <a class="p-works_item__link" href="<?php echo SCF::get('url') ?>" target="_blank" rel="noopener noreferrer"><img class="p-works_item__link_img" src="<?php the_post_thumbnail_url(); ?>" alt="<?php the_title(); ?>"> </a>
               <?php endif ?>
-                <p class="p-works-item-txt__heading">[制作時期]</p>
-                <p><?php echo SCF::get('year'); ?>年</p>
-                <p class="p-works-item-txt__heading">[使用技術]</p>
-                <?php
-                $technologies = SCF::get('technology'); ?>
-                <ul class="p-works-item-txt__list">
-                  <?php foreach ($technologies as $technology) : ?>
-                    <li class="p-works-item-txt__list_item"><?php echo esc_html($technology); ?></li>
-                  <?php endforeach; ?>
-                </ul>
-                <?php if (SCF::get('git') !== "") : ?>
-                  <p class="p-works-item-txt__heading">[Git]</p>
-                  <p><a class="p-works-item-txt__link" href="<?php echo SCF::get('git'); ?>" target="_blank" rel="noopener noreferrer"><?php echo SCF::get('git'); ?></a></p>
-                <?php endif; ?>
-                <p class="p-works-item-txt__heading">[コメント]</p>
-                <p><?php echo SCF::get('comment'); ?></p>
+              <p class="p-works-item-txt__heading">[制作時期]</p>
+              <p><?php echo SCF::get('year'); ?>年</p>
+              <p class="p-works-item-txt__heading">[使用技術]</p>
+              <?php
+              $technologies = SCF::get('technology'); ?>
+              <ul class="p-works-item-txt__list">
+                <?php foreach ($technologies as $technology) : ?>
+                  <li class="p-works-item-txt__list_item"><?php echo esc_html($technology); ?></li>
+                <?php endforeach; ?>
+              </ul>
+              <?php if (SCF::get('git') !== "") : ?>
+                <p class="p-works-item-txt__heading">[Git]</p>
+                <p><a class="p-works-item-txt__link" href="<?php echo SCF::get('git'); ?>" target="_blank" rel="noopener noreferrer"><?php echo SCF::get('git'); ?></a></p>
+              <?php endif; ?>
+              <p class="p-works-item-txt__heading">[コメント]</p>
+              <p><?php echo SCF::get('comment'); ?></p>
 
-                <?php if (SCF::get('basic_id') !== "") : ?>
-                  <p class="p-works-item-txt__heading">[Basic認証]</p>
-                  <p>ID : <?php echo SCF::get('basic_id'); ?>/ PW : <?php echo SCF::get('basic_pw'); ?></p>
-                <?php endif ?>
+              <?php if (SCF::get('basic_id') !== "") : ?>
+                <p class="p-works-item-txt__heading">[Basic認証]</p>
+                <p>ID : <?php echo SCF::get('basic_id'); ?>/ PW : <?php echo SCF::get('basic_pw'); ?></p>
+              <?php endif ?>
 
             </article>
           <?php endwhile; ?>
@@ -138,87 +194,172 @@
 
       <div id="mw_wp_form_mw-wp-form-459" class="mw_wp_form mw_wp_form_input">
 
-        <form class="p-form" method="post" action="" enctype="multipart/form-data">
-          <table class="p-contact-body">
-            <tbody>
-              <tr class="p-input-item">
-                <th class="p-input-item__inner">
-                  <p class="p-input-item__inner-title">会社名</p>
-                  <p><span class="p-input-item__inner-req">必須</span></p>
-                </th>
-                <td class="p-input-info">
-                  <input type="text" name="company-name" class="p-input-info__txt" size="60" value="" placeholder="例）株式会社ABC">
-                </td>
-              </tr>
-              <tr class="p-input-item">
-                <th class="p-input-item__inner">
-                  <p class="p-input-item__inner-title">担当者名</p>
-                  <p><span class="p-input-item__inner-req">必須</span></p>
-                </th>
-                <td class="p-input-info">
-                  <input type="text" name="pic" class="p-input-info__txt" size="60" value="" placeholder="例）山田太郎">
-                </td>
-              </tr>
-              <tr class="p-input-item">
-                <th class="p-input-item__inner">
-                  <p class="p-input-item__inner-title">担当者名(フリガナ)</p>
-                  <p><span class="p-input-item__inner-req">必須</span></p>
-                </th>
-                <td class="p-input-info">
-                  <input type="text" name="pic-kana" class="p-input-info__txt" size="60" value="" placeholder="例）ヤマダタロウ">
-                </td>
-              </tr>
-              <tr class="p-input-item">
-                <th class="p-input-item__inner">
-                  <p class="p-input-item__inner-title">メールアドレス</p>
-                  <p><span class="p-input-item__inner-req">必須</span></p>
-                </th>
-                <td class="p-input-info">
+        <?php if ($_SESSION['formStatus'] == 'input') : ?>
+          <!-- 入力フォームのHTML -->
+          <form class="p-form" method="post" action="" enctype="multipart/form-data">
+            <table class="p-contact-body">
+              <tbody>
+                <tr class="p-input-item">
+                  <th class="p-input-item__inner">
+                    <p class="p-input-item__inner-title">会社名</p>
+                    <p><span class="p-input-item__inner-req">必須</span></p>
+                  </th>
+                  <td class="p-input-info">
+                    <input type="text" name="company-name" class="p-input-info__txt" size="60" value="<?php echo htmlspecialchars(getSavedValue('company-name')); ?>" placeholder="例）株式会社ABC">
+                  </td>
+                </tr>
+                <tr class="p-input-item">
+                  <th class="p-input-item__inner">
+                    <p class="p-input-item__inner-title">担当者名</p>
+                    <p><span class="p-input-item__inner-req">必須</span></p>
+                  </th>
+                  <td class="p-input-info">
+                    <input type="text" name="pic" class="p-input-info__txt" size="60" value="<?php echo htmlspecialchars(getSavedValue('pic')); ?>" placeholder="例）山田太郎">
+                  </td>
+                </tr>
+                <tr class="p-input-item">
+                  <th class="p-input-item__inner">
+                    <p class="p-input-item__inner-title">担当者名(フリガナ)</p>
+                    <p><span class="p-input-item__inner-req">必須</span></p>
+                  </th>
+                  <td class="p-input-info">
+                    <input type="text" name="pic-kana" class="p-input-info__txt" size="60" value="<?php echo htmlspecialchars(getSavedValue('pic-kana')); ?>" placeholder="例）ヤマダタロウ">
+                  </td>
+                </tr>
+                <tr class="p-input-item">
+                  <th class="p-input-item__inner">
+                    <p class="p-input-item__inner-title">メールアドレス</p>
+                    <p><span class="p-input-item__inner-req">必須</span></p>
+                  </th>
+                  <td class="p-input-info">
 
-                  <input type="email" name="email" class="p-input-info__txt" size="60" value="" placeholder="例）mail@example.com">
-                </td>
-              </tr>
-              <tr class="p-input-item">
-                <th class="p-input-item__inner">
-                  <p class="p-input-item__inner-title">電話番号</p>
-                </th>
-                <td class="p-input-info">
-                  <div class="mwform-tel-field p-input-tell">
-                    <input type="text" name="tell[data][0]" class="p-input-tell__number" size="6" maxlength="5" value="">
-                    - <input type="text" name="tell[data][1]" class="p-input-tell__number" size="5" maxlength="4" value="">
-                    - <input type="text" name="tell[data][2]" class="p-input-tell__number" size="5" maxlength="4" value="">
-                  </div>
+                    <input type="email" name="email" class="p-input-info__txt" size="60" value="<?php echo htmlspecialchars(getSavedValue('email')); ?>" placeholder="例）mail@example.com">
+                  </td>
+                </tr>
+                <tr class="p-input-item">
+                  <th class="p-input-item__inner">
+                    <p class="p-input-item__inner-title">電話番号</p>
+                  </th>
+                  <td class="p-input-info">
+                    <div class="mwform-tel-field p-input-tell">
+                      <input type="text" name="tel-1" class="p-input-tell__number" size="6" maxlength="5" value="<?php echo htmlspecialchars(getSavedValue('tel-1')); ?>">
+                      - <input type="text" name="tel-2" class="p-input-tell__number" size="5" maxlength="4" value="<?php echo htmlspecialchars(getSavedValue('tel-2')); ?>">
+                      - <input type="text" name="tel-3" class="p-input-tell__number" size="5" maxlength="4" value="<?php echo htmlspecialchars(getSavedValue('tel-3')); ?>">
+                    </div>
 
-                  <input type="hidden" name="tell[separator]" value="-">
-                </td>
-              </tr>
-              <tr class="p-input-item">
-                <th class="p-input-item__inner">
-                  <p class="p-input-item__inner-title">お問い合わせ内容</p>
-                  <p><span class="p-input-item__inner-req">必須</span></p>
-                </th>
-                <td class="p-input-info">
-                  <textarea name="msg" class="p-input-info__textarea" cols="30" rows="5"></textarea>
-                </td>
-              </tr>
-            </tbody>
-          </table>
+                    <input type="hidden" name="tell[separator]" value="-">
+                  </td>
+                </tr>
+                <tr class="p-input-item">
+                  <th class="p-input-item__inner">
+                    <p class="p-input-item__inner-title">お問い合わせ内容</p>
+                    <p><span class="p-input-item__inner-req">必須</span></p>
+                  </th>
+                  <td class="p-input-info">
+                  <textarea name="msg" class="p-input-info__textarea" cols="30" rows="5"><?php echo htmlspecialchars(getSavedValue('msg')); ?></textarea>
+                  </td>
+                </tr>
+              </tbody>
+            </table>
 
-          <!-- <div class="c-submit">
-            <input type="submit" name="submitConfirm" value="確認画面へ" class="c-submit__button">
-          </div> -->
+            <div class="c-submit">
+              <input type="submit" name="submitConfirm" value="確認画面へ" class="c-submit__button" onclick="navigateToContact();">
+            </div>
+          </form>
+
+        <?php elseif ($_SESSION['formStatus'] == 'confirm') : ?>
+          <!-- 確認画面のHTML -->
+          <form class="p-form" method="post" action="" enctype="multipart/form-data">
+            <table class="p-contact-body">
+              <tbody>
+                <tr class="p-input-item">
+                  <th class="p-input-item__inner">
+                    <p class="p-input-item__inner-title">会社名</p>
+                    <p><span class="p-input-item__inner-req">必須</span></p>
+                  </th>
+                  <td class="p-input-info">
+                  <p class="p-input-info__txt"><?php echo $companyName ?></p>
+                    <input type="hidden" name="company-name" class="p-input-info__txt" size="60" value="<?php echo htmlspecialchars(getSavedValue('company-name')); ?>" placeholder="例）株式会社ABC">
+                  </td>
+                </tr>
+                <tr class="p-input-item">
+                  <th class="p-input-item__inner">
+                    <p class="p-input-item__inner-title">担当者名</p>
+                    <p><span class="p-input-item__inner-req">必須</span></p>
+                  </th>
+                  <td class="p-input-info">
+                  <p class="p-input-info__txt"><?php echo $pic; ?></p>
+                    <input type="hidden" name="pic" class="p-input-info__txt" size="60" value="<?php echo htmlspecialchars(getSavedValue('pic')); ?>" placeholder="例）山田太郎">
+                  </td>
+                </tr>
+                <tr class="p-input-item">
+                  <th class="p-input-item__inner">
+                    <p class="p-input-item__inner-title">担当者名(フリガナ)</p>
+                    <p><span class="p-input-item__inner-req">必須</span></p>
+                  </th>
+                  <td class="p-input-info">
+                  <p class="p-input-info__txt"><?php echo $picKana; ?></p>
+                    <input type="hidden" name="pic-kana" class="p-input-info__txt" size="60" value="<?php echo htmlspecialchars(getSavedValue('pic-kana')); ?>" placeholder="例）ヤマダタロウ">
+                  </td>
+                </tr>
+                <tr class="p-input-item">
+                  <th class="p-input-item__inner">
+                    <p class="p-input-item__inner-title">メールアドレス</p>
+                    <p><span class="p-input-item__inner-req">必須</span></p>
+                  </th>
+                  <td class="p-input-info">
+                  <p class="p-input-info__txt"><?php echo $email; ?></p>
+                    <input type="hidden" name="email" class="p-input-info__txt" size="60" value="<?php echo htmlspecialchars(getSavedValue('email')); ?>" placeholder="例）mail@example.com">
+                  </td>
+                </tr>
+                <tr class="p-input-item">
+                  <th class="p-input-item__inner">
+                    <p class="p-input-item__inner-title">電話番号</p>
+                  </th>
+                  <td class="p-input-info">
+                    <div class="mwform-tel-field p-input-tell">
+                    <span class="p-input-info__txt"><?php echo $tel1; ?>- </span>
+                    <span class="p-input-info__txt"><?php echo $tel2; ?>- </span>
+                    <span class="p-input-info__txt"><?php echo $tel3; ?></span>
+
+                      <input type="hidden" name="tel-1" class="p-input-tell__number" size="6" maxlength="5" value="<?php echo htmlspecialchars(getSavedValue('tel-1')); ?>">
+                       <input type="hidden" name="tel-2" class="p-input-tell__number" size="5" maxlength="4" value="<?php echo htmlspecialchars(getSavedValue('tel-2')); ?>">
+                       <input type="hidden" name="tel-3" class="p-input-tell__number" size="5" maxlength="4" value="<?php echo htmlspecialchars(getSavedValue('tel-3')); ?>">
+                    </div>
+
+                    <!-- <input type="hidden" name="tell[separator]" value="-"> -->
+                  </td>
+                </tr>
+                <tr class="p-input-item">
+                  <th class="p-input-item__inner">
+                    <p class="p-input-item__inner-title">お問い合わせ内容</p>
+                    <p><span class="p-input-item__inner-req">必須</span></p>
+                  </th>
+                  <td class="p-input-info">
+                    <p><?php echo $msg; ?></p>
+                    <input type="hidden" value="$msg" name="msg">
+                    <!-- <textarea name="msg" class="p-input-info__textarea" cols="30" rows="5" value="<?php echo htmlspecialchars(getSavedValue('')); ?>"></textarea> -->
+                  </td>
+                </tr>
+              </tbody>
+            </table>
+
+          </form>
+
           <div class="c-submit">
+            <form action="" method="post">
+              <button type="submit" name="submitBack" value="back" class="c-submit__button">戻る</button>
+              <br>
 
-            <button type="submit" name="submitBack" value="back" class="c-submit__button">戻る</button>
-            <br>
-
-            <input type="submit" name="mwform_submitButton-36" value="送信する" class="c-submit__button">
-
+              <input type="submit" name="submitFinal" value="送信する" class="c-submit__button" onclick="navigateToContact();">
+            </form>
           </div>
 
+        <?php elseif ($_SESSION['formStatus'] == 'complete') : ?>
+          <!-- 送信完了メッセージのHTML -->
+          <p>フォームが送信されました。</p>
 
-        </form>
-        <!-- end .mw_wp_form -->
+        <?php endif; ?>
 
 
       </div>
@@ -229,3 +370,10 @@
 </main>
 
 <?php get_footer(); ?>
+
+<script>
+function navigateToContact() {
+    // `contact`セクションにスクロールする
+    document.getElementById('contact').scrollIntoView();
+}
+</script>
